af7b03fc833393464b810b2550e063febfd3eeff
Timo Portal <timo.portal@ceff.ch>
2022-02-10 10:45:57 +0100

    1.2.2

--- check.php
+++ check.php
@@ -1,5 +1,15 @@
 <pre>
 <?php
-print_r($_POST);
+require("./class/Personne.class.php");
+
+$per = new Personne();
+
+$per->set_nom("Saquet");
+$per->set_prenom("Bilbo");
+$per->set_email("bilbo.saquet@naz.ger");
+echo $per->set_password("Pa/w0rd$");
+echo $per->set_news_letter(1);
+
+echo $per;
 ?>
 </pre>

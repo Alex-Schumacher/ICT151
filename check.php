cc6c0d185c78ee3dcd81e75fb176386f9e936ada
Timo Portal <timo.portal@ceff.ch>
2022-02-10 11:30:32 +0100

    1.2.3

--- check.php
+++ check.php
@@ -1,5 +1,6 @@
 <pre>
 <?php
+require ("./config/config.inc.php");
 require("./class/Personne.class.php");
 
 $per = new Personne();
---
+++ config/config.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+define("BASE_NAME", "ICT151");
+define("SQL_HOST", "localhost");
+define("SQL_USER", "root");
+define("SQL_PASSWORD", "");
+
+?>
